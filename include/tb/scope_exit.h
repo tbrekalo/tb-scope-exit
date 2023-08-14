d1304c8ff5abefb5d3da5ec957f925eed03d6cb2
tbrekalo <t@tbrekalo.xyz>
2023-08-14 16:38:24 +0200

    impl: basic scope_exit

--- include/tb/scope_exit.h
+++ include/tb/scope_exit.h
@@ -1,6 +1,54 @@
 #ifndef TB_SCOPE_EXIT_H_
 #define TB_SCOPE_EXIT_H_
 
-namespace tb {}
+#include <concepts>
+#include <exception>
+#include <type_traits>
+#include <utility>
+
+namespace tb::detail {
+
+class exit_policy {};
+
+template <class ExitFn, class ExitPolicy = exit_policy>
+  requires(std::is_nothrow_invocable_r_v<void, ExitFn>)
+class scope_guard {
+  ExitFn exit_fn_;
+  bool enabled_;
+
+ public:
+  template <class Fn>
+  explicit scope_guard(Fn&& fn) noexcept
+      : exit_fn_(std::forward<Fn>(fn)), enabled_(true) {}
+
+  scope_guard(scope_guard const& that) = delete;
+  auto operator=(scope_guard const& that) -> scope_guard& = delete;
+
+  scope_guard(scope_guard&& that) noexcept
+      : exit_fn_(std::move(that.exit_fn_)),
+        enabled_(std::exchange(that.enabled_), false) {}
+  auto operator=(scope_guard&& that) = delete;
+
+  ~scope_guard() {
+    if (enabled_) {
+      exit_fn_();
+    }
+  }
+};
+
+}  // namespace tb::detail
+
+namespace tb {
+
+template <class Fn>
+using scope_exit = detail::scope_guard<Fn>;
+
+template <class ExitFn>
+auto make_scope_exit(ExitFn&& exit_fn) {
+  return scope_exit<std::remove_reference_t<ExitFn>>(
+      std::forward<ExitFn>(exit_fn));
+}
+
+}  // namespace tb
 
 #endif /* TB_SCOPE_EXIT_H_ */
--- test/src/main.cc
+++ test/src/main.cc
@@ -1,3 +1,36 @@
 #include "catch2/catch_test_macros.hpp"
+#include "tb/scope_exit.h"
 
-TEST_CASE("basic") { REQUIRE(2 == 2); }
+// exception class used for tests
+class strong_exception : std::runtime_error {
+ public:
+  strong_exception() : std::runtime_error("") {}
+  explicit strong_exception(std::string const& what)
+      : std::runtime_error(what) {}
+};
+
+TEST_CASE("tb::scope_exit") {
+  auto x = 0U;
+  SECTION("tb::scope_exit nothrow") {
+    REQUIRE_NOTHROW([&x]() -> void {
+      auto scope_exit =
+          tb::make_scope_exit([&x]() noexcept -> void { x = 42; });
+      CHECK(x == 0U);
+    }());
+  }
+
+  SECTION("tb::scope_exit throws") {
+    REQUIRE_THROWS_AS(
+        [&x]() -> void {
+          auto scope_exit =
+              tb::make_scope_exit([&x]() noexcept -> void { x = 42U; });
+          CHECK(x == 0U);
+
+          throw strong_exception();
+        }(),
+        strong_exception);
+  }
+
+  REQUIRE(x == 42U);
+}
+
